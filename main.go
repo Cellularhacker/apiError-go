12d93026b267aba7b20dd0d434ebedc6fcf7f34c
Cellularhacker <wva3cdae@gmail.com>
2024-07-25 09:39:08 +0900

    Updated template

--- main.go
+++ main.go
@@ -2,16 +2,58 @@ package apiError
 
 import (
 	"fmt"
-	"github.com/pkg/errors"
 	"net/http"
 	"strings"
 )
 
 const (
-	UnknownServerError = 5000
+	UnknownServerError                 = 5000
+	Unauthorized                       = 5001
+	EmailAlreadyRegistered             = 5002
+	PasswordLessThanRequired           = 5003
+	SomethingMissing                   = 5004
+	WrongConfirmationCode              = 5005
+	WrongAccessToken                   = 5006
+	EmailNotRegistered                 = 5007
+	EmailAlreadyConfirmed              = 5008
+	WrongPassword                      = 5009
+	DuplicateAPIKey                    = 5010
+	WrongEmailOrPassword               = 5011
+	NameAlreadyRegistered              = 5012
+	UserDeleted                        = 5013
+	ContentMissing                     = 5014
+	NoLeaguePresent                    = 5015
+	CantUnfollowLOT                    = 5016
+	ReferralCodeMissing                = 5017
+	EmailBelongsToAnotherAccount       = 5018
+	PhoneNumberBelongsToAnotherAccount = 5019
+
+	BotBalanceLessThanMinimum = 5031
+	BotKeyCantDelete          = 5032
+	BotCompanyError           = 5033
+	BotDoesntExistError       = 5034
+
+	FunctionNotSupportedYetForGivenExchange = 5035
+	InvoicePaymentVerifyUnknownError        = 5036
+	KeyHasNoPermissionForGivenOperation     = 5037
+
+	LeagueRegisterByPassed      = 5040
+	LeagueMinimumBalanceNotMet  = 5041
+	LeagueRemovedDueToKeyDelete = 5042
+
+	ErrorBotRatioDifferenceHigh      = 6001
+	ErrorBotCopyingKeyInvalidated    = 6002
+	ErrorBotOriginalKeyInvalidated   = 6003
+	ErrorBotPositionMismatch         = 6004
+	ErrorBotUnknown                  = 6005
+	ErrorBotOpenOrderFound           = 6006
+	ErrorKeyIsRegisteredInAnotherBot = 6007
+	ErrorKeyIsNotCopyLeader          = 6008
+	ErrorCopyLeaderConditionsNotMet  = 6009
+	ErrorBotBalanceNotSufficient     = 6010
 )
 
-// Error error returned by APIHandler
+// Error APIError error returned by APIHandler
 type Error struct {
 	Error   error
 	Message string
@@ -38,7 +80,7 @@ func DetectError(err error) *Error {
 }
 
 func InternalServerErrorLoc(loc string, err error) *Error {
-	return &Error{errors.New(fmt.Sprintf("%s: %s", loc, err)), "Unknown error! Try again", UnknownServerError}
+	return InternalServerError(fmt.Errorf("%s: %s", loc, err))
 }
 
 func InternalServerError(err error) *Error {
@@ -57,11 +99,11 @@ func NotAuthorizedError(message string) *Error {
 	return &Error{nil, message, http.StatusUnauthorized}
 }
 func NotAuthorizedUser() *Error {
-	return NotFoundError("unauthorized")
+	return NotAuthorizedError("unauthorized")
 }
 
 func SomethingMissingError(message string) *Error {
-	return &Error{nil, message, http.StatusBadRequest}
+	return &Error{nil, message, SomethingMissing}
 }
 
 func NotFoundError(message string) *Error {
